016ad072a305d20a5988a5528d54fdd81617f5e4
g9militantsYT <g9militants@gmail.com>
2024-01-06 21:46:29 +0530

    support button

--- Builder/src/components/footer/footer.tsx
+++ Builder/src/components/footer/footer.tsx
@@ -5,8 +5,14 @@ export default component$(() => {
   return (
     <footer>
       <div class={styles.wrapper}>
-        <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
+        <a class={styles.anchor}>
           <span>Made with ♡ by G9 Aerospace</span>
+        </a>
+        <a href="https://www.buymeacoffee.com/g9aerospace">
+          <img
+            src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=g9aerospace&button_colour=5F7FFF&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00"
+            alt="Buy me a coffee"
+          />
         </a>
       </div>
     </footer>
