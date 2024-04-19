0583e2a1e4ad4c2a3fd21696cb497bc1c3ddb971
G9 Aerospace <g9militants@gmail.com>
2024-04-19 16:33:34 +0530

    Update CSS styles for sections and items in global.css, servers.module.css, hosts.module.css, and creations.module.css

--- src/components/starter/creations/creations.tsx
+++ src/components/starter/creations/creations.tsx
@@ -2,26 +2,74 @@ import { component$ } from "@builder.io/qwik";
 import styles from "./creations.module.css";
 
 import websiteicon from "../../../media/website-icon.png"
-import discordicon from "../../../media/discord-icon.png"
+import githubIcon from "../../../media/discord-icon.png"
 
-const organisations = [
+const creations = [
   {
-    name: "Bricks Website",
-    organisation: "Bricks Network",
-    website: "https://bricks-network.xyz",
-    discordLink: "https://discord.gg/uRuPDJ5j",
+    name: "Monitor",
+    organisation: "EraNodes",
+    website: "https://status.eranodes.xyz",
+    githubLink: "https://github.com/Eranodes/Monitor",
   },
   {
-    name: "Dragon Network Website",
-    organisation: "Dragon Network",
-    website: "https://dragonnetwork.digital",
-    discordLink: "https://discord.gg/ukzxwAujZM",
+    name: "EraNodes Utils Bot",
+    organisation: "EraNodes",
+    website: "",
+    githubLink: "https://github.com/Eranodes/EraNodes-Utils",
   },
   {
-    name: "CraftersGG Websute",
-    organisation: "CraftersGG",
-    website: "https://craftersgg.site/",
-    discordLink: "https://discord.gg/CwWbWm5FKq",
+    name: "Craft File Reader",
+    organisation: "KSPCommunity",
+    website: "https://www.npmjs.com/package/@kspcommunity/craft-file-reader",
+    githubLink: "https://github.com/kspcommunity/Craft-File-Reader",
+  },
+  {
+    name: "KSP Community Website",
+    organisation: "KSPCommunity",
+    website: "https://kspcommunity.com/",
+    githubLink: "https://github.com/kspcommunity/kspcommunity.com",
+  },
+  {
+    name: "KSP Community Discord Bot",
+    organisation: "KSPCommunity",
+    website: "",
+    githubLink: "https://github.com/kspcommunity/KSP-Community-Bot",
+  },
+  {
+    name: "KSP Mod Parts List",
+    organisation: "KSPCommunity",
+    website: "https://mod-parts.kspcommunity.com/data.json",
+    githubLink: "https://github.com/kspcommunity/Mod-Parts-Lister",
+  },
+  {
+    name: "MoonRock",
+    organisation: "VyperGroup",
+    website: "https://github.com/VyperGroup/Moonrock",
+    githubLink: "https://github.com/VyperGroup/Moonrock",
+  },
+  {
+    name: "g9aerospace.in",
+    organisation: "",
+    website: "https://g9aerospace.in",
+    githubLink: "https://github.com/g9militantsYT/g9aerospace.in",
+  },
+  {
+    name: "Phoenix",
+    organisation: "",
+    website: "",
+    githubLink: "https://github.com/g9militantsYT/Phoenix",
+  },
+  {
+    name: "BBN Resource Calculator",
+    organisation: "BBN",
+    website: "https://bbn.g9aerospace.in/",
+    githubLink: "https://github.com/g9militantsYT/BBN-Resource-Calculator",
+  },
+  {
+    name: "Minecraft Server Showcase Bot",
+    organisation: "",
+    website: "",
+    githubLink: "https://github.com/g9militantsYT/Minecraft-Server-Showcase",
   },
 ];
 
@@ -30,7 +78,7 @@ export default component$(() => {
   return (
     <div class="container">
       <ul class={styles.creationList}>
-        {organisations.map((creation, index) => (
+        {creations.map((creation, index) => (
           <li key={index} class={`${styles.creationItem} card`}>
             <div class="name">{creation.name}</div>
             <div class="content">
@@ -38,13 +86,11 @@ export default component$(() => {
               <p>
                 <a href={creation.website} target="_blank" rel="noopener noreferrer">
                   <img src={websiteicon} alt="Website Icon" class={`${styles.websiteIcon} websiteIcon`} />
-                  {creation.website}
                 </a>
               </p>
               <p>
-                <a href={creation.discordLink} target="_blank" rel="noopener noreferrer">
-                  <img src={discordicon} alt="Discord Icon" class={`${styles.discordIcon} discordIcon`} />
-                  {creation.discordLink}
+                <a href={creation.githubLink} target="_blank" rel="noopener noreferrer">
+                  <img src={githubIcon} alt="Discord Icon" class={`${styles.githubIcon} githubIcon`} />
                 </a>
               </p>
             </div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,21 +17,21 @@ export default component$(() => {
       <div role="presentation" class="ellipsis"></div>
       <div role="presentation" class="ellipsis ellipsis-purple"></div>
       
-      <div class="container container-center container-spacing-xl">
+      <div class="container container-center">
         <h3>
           You can count on these <span class="highlight">Hosts</span>
         </h3>
         <Hosts />
       </div>
 
-      <div class="container container-center container-spacing-xl">
+      <div class="container container-center">
         <h3>
           You can play on these <span class="highlight">Servers</span>
         </h3>
         <Servers />
       </div>
 
-      <div class="container container-center container-spacing-xl">
+      <div class="container container-center">
         <h3>
           You can check out these <span class="highlight">Creations</span>
         </h3>
