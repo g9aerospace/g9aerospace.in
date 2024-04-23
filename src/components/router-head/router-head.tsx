8bb2d341ff93f9dcc4b8e6badaf760f1822a06db
G9 Aerospace <g9militants@gmail.com>
2024-04-23 20:51:01 +0530

    Theme color

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,6 +15,7 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#2e7bff" /> 
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
       {head.meta.map((m) => (
