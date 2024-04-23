ca22eda7f83f4b8a2a6a6f27e4cd56d2e11922ec
G9 Aerospace <g9militants@gmail.com>
2024-04-23 14:54:54 +0530

    Update sidebar component and related styles

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,6 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import styles from "./sidebar.module.css";
 import g9aerospacelogo from "../../media/g9aerospace.png";
 import hostsData from "../../data/hosts.json";
+import serversData from "../../data/servers.json"
 
 export default component$(() => {
 
@@ -27,11 +28,19 @@ export default component$(() => {
                 <button class={styles.dropbtn}>Hosts</button>
                 <div class={styles.dropdownContent}>
                   {hostsData.map(host => (
-                    <a key={host.id} href={`/hosts/${host.name.toLowerCase()}`} class={styles.dropdownItem}>{host.name}</a>
+                    <a key={host.name} href={`/hosts/${host.name.toLowerCase()}`} class={styles.dropdownItem}>{host.name}</a>
                   ))}
                 </div>
               </div>
-              <li><a href="/#servers">Servers</a></li>
+              {/* Servers Section */}
+              <div class={styles.dropdown}>
+                <button class={styles.dropbtn}>Servers</button>
+                <div class={styles.dropdownContent}>
+                  {serversData.map(server => (
+                    <a key={server.name} href={`/servers/${server.name.toLowerCase()}`} class={styles.dropdownItem}>{server.name}</a>
+                  ))}
+                </div>
+              </div>
               <li><a href="/#creations">Creations</a></li>
             </ul>
           </div>
