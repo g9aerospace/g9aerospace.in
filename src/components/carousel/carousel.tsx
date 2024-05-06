2d480fcc65abaa1993ebd3d54c631640833931e2
G9 Aerospace <g9militants@gmail.com>
2024-05-06 15:37:08 +0530

    🛠️ carousel.tsx -> Updated image imports and added new images 🟢 embernodes.png 🟢 hostys-connect.png

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable qwik/jsx-img */
 import { component$ } from "@builder.io/qwik";
 import styles from "./carousel.module.css";
 
-// Import all images from the media folder
+// Import images from the media folder
 import bbn from "../../media/bbn.png";
 import eranodes from "../../media/eranodes.png";
 import legacynodes from "../../media/legacynodes.png";
@@ -9,9 +10,12 @@ import dimehosting from "../../media/dimehosting.png";
 import destinynodes from "../../media/destinynodes.png";
 import craftersgg from "../../media/craftersgg.png"
 import dragonnetwork from "../../media/dragon-network.png";
-import vypergroup from "../../media/vypergroup.png"
+import vypergroup from "../../media/vypergroup.png";
+import embernodes from "../../media/embernodes.png";
+import bricksnetwork from "../../media/bricks-network.png";
+import hostysconnect from "../../media/hostys-connect.png"
 
-const images = [bbn, eranodes, vypergroup, legacynodes, dimehosting, destinynodes, craftersgg, dragonnetwork];
+const images = [bbn, eranodes, vypergroup, legacynodes, embernodes, dimehosting, hostysconnect, destinynodes, bricksnetwork, craftersgg, dragonnetwork];
 // Define the Qwik component for the carousel
 export default component$(() => {
 
